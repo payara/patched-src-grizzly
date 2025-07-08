0340697cec80b1f89595ec55231204babe708c74
Alfonso Altamirano <alfonso.altamirano@payara.fish>
2025-07-08 09:45:03 -0600

    FISH-11415: adding validation for improper management of headers

--- modules/http/src/main/java/org/glassfish/grizzly/http/HttpServerFilter.java
+++ modules/http/src/main/java/org/glassfish/grizzly/http/HttpServerFilter.java
@@ -561,7 +561,11 @@ public class HttpServerFilter extends HttpCodecFilter {
         }
 
         final MimeHeaders headers = request.getHeaders();
-
+        //here qe can add validation:
+        if (headers.contains(Header.ContentLength.getLowerCase()) && headers.contains(Header.TransferEncoding.getLowerCase())) {
+            request.getProcessingState().error = true;
+            return;
+        }
         DataChunk hostDC = null;
 
         // Check for a full URI (including protocol://host:port/)
