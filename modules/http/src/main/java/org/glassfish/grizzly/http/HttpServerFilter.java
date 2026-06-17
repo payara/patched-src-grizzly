e1df10a6cbaa69b5c779c008ccc4f494340c8589
Andrew James Pielage <andrew.pielage@azul.com>
2026-06-17 16:34:06 +0100

    Revert "FISH-11415: adding log and fixing comments from pr"
    This reverts commit 9361c9d9eeb8b007ccbc1dc0d5c8d8ad18e0f520.

--- modules/http/src/main/java/org/glassfish/grizzly/http/HttpServerFilter.java
+++ modules/http/src/main/java/org/glassfish/grizzly/http/HttpServerFilter.java
@@ -16,8 +16,6 @@
 
 package org.glassfish.grizzly.http;
 
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import org.glassfish.grizzly.http.util.ContentType;
 import org.glassfish.grizzly.filterchain.FilterChainEvent;
 import org.glassfish.grizzly.http.util.Constants;
@@ -58,7 +56,6 @@ import org.glassfish.grizzly.http.util.HttpUtils;
 public class HttpServerFilter extends HttpCodecFilter {
     public static final String HTTP_SERVER_REQUEST_ATTR_NAME =
             HttpServerFilter.class.getName() + ".HttpRequest";
-    private final static Logger LOGGER = Grizzly.logger(HttpServerFilter.class);
 
     public static final FilterChainEvent RESPONSE_COMPLETE_EVENT =
             new HttpEvents.ResponseCompleteEvent();
@@ -634,9 +631,8 @@ public class HttpServerFilter extends HttpCodecFilter {
         
         final MimeHeaders headers = request.getHeaders();
 
-        //here we can add validation to prevent Http Server invalid use of headers
-        if (headers.contains(Header.ContentLength) && headers.contains(Header.TransferEncoding)) {
-            LOGGER.log(Level.SEVERE, "Can't use both headers Content-Length and Transfer-Encoding from the same request");
+        //here qe can add validation:
+        if (headers.contains(Header.ContentLength.getLowerCase()) && headers.contains(Header.TransferEncoding.getLowerCase())) {
             request.getProcessingState().error = true;
             return;
         }
