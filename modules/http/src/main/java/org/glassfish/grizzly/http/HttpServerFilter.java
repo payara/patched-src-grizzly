b14134cd40cda5b5ae06b1912ab39cda62a52068
Andrew James Pielage <andrew.pielage@azul.com>
2026-06-17 16:33:04 +0100

    Revert "FISH-11415: adding validation for improper management of headers"
    This reverts commit acb915b141e2d022f83577f492976e5429856512.

--- modules/http/src/main/java/org/glassfish/grizzly/http/HttpServerFilter.java
+++ modules/http/src/main/java/org/glassfish/grizzly/http/HttpServerFilter.java
@@ -561,11 +561,7 @@ public class HttpServerFilter extends HttpCodecFilter {
         }
 
         final MimeHeaders headers = request.getHeaders();
-        //here qe can add validation:
-        if (headers.contains(Header.ContentLength.getLowerCase()) && headers.contains(Header.TransferEncoding.getLowerCase())) {
-            request.getProcessingState().error = true;
-            return;
-        }
+
         DataChunk hostDC = null;
 
         // Check for a full URI (including protocol://host:port/)
